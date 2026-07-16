60dcd52cca67f0c08e444f7de065b11586de1e83
Lizc233 <lizhaoc2021@163.com>
2026-07-16 12:33:15 +0800

    init

---
+++ src/main/java/com/educationcertificationsystem/EducationCertificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.educationcertificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationCertificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationCertificationSystemApplication.class, args);
+    }
+
+}
